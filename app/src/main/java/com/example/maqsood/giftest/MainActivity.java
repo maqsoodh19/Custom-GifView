8fbcbb8b78020c199bc6e018608dde597ed10870
Maqsood <maqsoodh19@gmail.com>
2019-06-14 01:20:29 +0500

    'ksjnf'

---
+++ app/src/main/java/com/example/maqsood/giftest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.maqsood.giftest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    PlayGifView playGifView;
+    GifImageView gifImageView;
+    EasyGifView easyGifView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        easyGifView = findViewById(R.id.gifView);
+        easyGifView.setGifFromResource(R.drawable.cycle_gif);
+    }
+}
